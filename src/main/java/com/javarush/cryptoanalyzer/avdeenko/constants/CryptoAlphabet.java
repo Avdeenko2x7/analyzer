3a9b3b66c134f0c0c54a4dd5761c0f232e38459b
Avdieienko Oleksandr <avdeenko8535@gmail.com>
2023-03-05 17:35:18 +0200

    CryptoAlphabet.java added

---
+++ src/main/java/com/javarush/cryptoanalyzer/avdeenko/constants/CryptoAlphabet.java
@@ -0,0 +1,12 @@
+package com.javarush.cryptoanalyzer.avdeenko.constants;
+
+public class CryptoAlphabet {
+    private static final String lettersUpperCase = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    private static final String lettersLowerCase = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    private static final String numbers = "0123456789";
+    private static final String symbols = ".,:\"\"-!? ";
+    public static final String ALPHABET = lettersUpperCase + lettersLowerCase + numbers + symbols;
+
+    public static final int ALPHABET_LENGTH = ALPHABET.length();
+}
+
